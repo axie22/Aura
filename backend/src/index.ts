8ff7aa4402e2745c7b2ead448d1cda288356e71e
axie22 <alexxie9667@gmail.com>
2026-01-18 12:10:15 -0500

    Resolve handler merge conflicts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use('/public', express.static('public'));
+
 app.use(express.json({
   verify: (req: any, res, buf) => {
     req.rawBody = buf;
